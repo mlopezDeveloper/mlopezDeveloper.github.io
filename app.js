65c94f6dc8c087f1383ef093d4fd24fed82b5f8c
Marcos LOPEZ <mlopez@CNP.com.ar>
2023-11-25 15:53:51 -0300

    First commit portafolio personal web v.1

---
+++ app.js
@@ -0,0 +1,15 @@
+let baslik = document.title;
+window.onblur = () =>
+	document.title = "Te sigo viendo...";
+window.onfocus = () =>
+	document.title = "Portafolio";
+
+    /*const nav = document.querySelector('barra');
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 0) {
+            nav.classList.add('barra-color');
+        } else {
+            nav.classList.remove('barra-color');
+        }
+    });*/
